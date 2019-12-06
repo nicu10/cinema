20895a0011639e7be780488e286d8537e251742b
nicu10 <46652276+nicu10@users.noreply.github.com>
2019-12-06 10:47:59 +0200

    Add files via upload

---
+++ Pages/books/read.php
@@ -0,0 +1,29 @@
+<?
+$result = mysqli_query($connection, "SELECT idBooks, Title, anul, photo FROM Books");
+?>
+<table class="table" >
+    <thead>
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+         <th>anul</th>
+         <th>Photo</th>
+           <th></th>
+    </tr>
+    </thead>
+    <tbody>
+        <?
+        while($element = mysqli_fetch_assoc($result)){
+            ?>
+            <tr>
+                <td><?=$element['idBooks'];?></td>
+                <td><?=$element['Title'];?></td>
+                 <td><?=$element['anul'];?></td>
+                 <td><img src="<?=$element['photo'];?>"></td>
+                 <td><a href="index.php?module=books&action=delete&idBooks=<?=$element['idBooks'];?>">Delete</a></td>
+            </tr>
+            <?
+        }?>
+    </tbody>
+
+</table>
