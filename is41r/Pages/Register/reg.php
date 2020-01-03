6057420cdbb9eddd91a0612f67c3a67bb5730466
nicu10 <46652276+nicu10@users.noreply.github.com>
2020-01-03 21:45:21 +0200

    Add files via upload

--- is41r/Pages/Register/reg.php
+++ is41r/Pages/Register/reg.php
@@ -1,38 +1,7 @@
-<?php
-$connection = mysqli_connect('localhost', 'root', '', 'cinema');
 
-$username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
-$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-$login = filter_var($_POST['login'], FILTER_SANITIZE_STRING);
-$pass = filter_var($_POST['pass'], FILTER_SANITIZE_STRING);
-
-  /*if(strlen($username)<=3)
-  exit();
-  else if(strlen($email)<=3)
-  exit();
-  else if(strlen($login)<=3)
-  exit();
-  else if(strlen($pass)<=3)
-  exit();
-
-  $hash = "qwdqwd124asfd";
-  $pass = md5($pass . $hash);*/
-
-
-$user = 'root';
-$password = '';
-$db = 'cinema';
-$host ='localhost';
-
-$dsn = 'mysql:host='.$host.';dbname='.$db;
-$pdo = new PDO($dsn, $user, $password);
-  $sql = 'INSERT INTO users(name,email,login,pass) VALUES(?,?,?,?)';
-  $query = $pdo->prepare($sql);
-  $query->execute([$username,$email,$login,$pass]);
-   ?>
 <div class="col-md-12">
   <h4>Forma de registrare</h4>
-  <form action="" method="post">
+  <form  method="post">
     <label for="username">Numele</label>
     <input type="text" name="username" id="username" class="form-control">
     <label for="email">Email</label>
@@ -43,9 +12,38 @@ $pdo = new PDO($dsn, $user, $password);
 
     <label for="pass">Parola</label>
     <input type="password" name="pass" id="pass" class="form-control">
-    <button type="submit" class="btn btn-success mt-5">
-
+<div class="alert alert-danger mt-2" id="errorBlock"></div>
+    <button type="button" id="reg_user" class="btn btn-success mt-5">
       Registrare
     </button>
   </form>
 </div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<script>
+
+$('#reg_user').click(function () {
+  var name = $('#username').val();
+  var email = $('#email').val();
+  var login = $('#login').val();
+  var pass = $('#pass').val();
+
+  $.ajax({
+    type: "POST",
+    url: "Pages/Register/regg.php",
+    cache: false,
+    data: {'username' : name, 'email' : email, 'login' : login, 'pass' : pass},
+    dataType: 'html',
+    success: function(data) {
+      if (data == 'terminat'){
+      $('#reg_user').text('registrare terminata');
+      $('#errorBlock').hide();
+      }
+      else{
+      $('#errorBlock').show();
+      $('#errorBlock').text(data);
+          }
+    }
+  });
+});
+</script>
